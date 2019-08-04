4d7835945bb0cb556af926cba6f6bfbb9c953635
orlandc <orlando.montenegro@correounivalle.edu.co>
2019-08-04 08:20:31 -0500

    Create raw text for bioinfer

--- parser.py
+++ parser.py
@@ -10,22 +10,11 @@ for r, d, f in os.walk(path):
         if '.xml' in file:
             files.append(os.path.join(r, file))
 
-def getWord(node):
-    oracion = ""
-    for parent in node.childNodes:
-        if parent.nodeType != parent.TEXT_NODE:
-            print(parent.getAttribute("origText"))
-            if parent.getAttribute("origText") != "":
-                oracion += parent.getAttribute("origText") + " "
-            else:
-                oracion += getWord(parent)
-    
-    return oracion
-
 for i, f in enumerate(files):
     
-    path2 = f.replace("BioInfer_corpus_1.1.1", "raw")
-    name2 = path2.replace(".xml", ".txt")
+    
+    path2 = f.replace("BioInfer/", "raw/")
+    name2 = path2.replace("_1.1.1.xml", "_raw.txt")
     if not os.path.exists(os.path.dirname(name2)):
         os.makedirs(os.path.dirname(name2))
     
@@ -35,9 +24,9 @@ for i, f in enumerate(files):
 
     doc = xml.dom.minidom.parse(f);
     sentence = doc.getElementsByTagName("sentence")
+    
     for tag in sentence:
-        print (getWord(tag))
-        #print(tag)
-        #file2.write(getWord(tag).encode('utf8') + "\n")
+        word = tag.getAttribute("origText")
+        file2.write(word + "\n")
     
     file2.close()
